f89b0fa2056ecbae09198034af55fde34baca340
beartime234 <josheatonis234@gmail.com>
2019-12-14 18:29:34 +1300

    Adding env variables

--- whos_that_pokemon_s3gallery/__init__.py
+++ whos_that_pokemon_s3gallery/__init__.py
@@ -7,6 +7,8 @@ module_dir = os.path.dirname(__file__)
 s3_bucket = os.environ["S3_BUCKET"]
 config = {}
 
+
+# Loads the config
 with open(f"{module_dir}/config.yml", 'r') as stream:
     try:
         config = yaml.safe_load(stream)
