25b2e25eb08724af76808265c4b92c08a50c3afe
beartime234 <josheatonis234@gmail.com>
2019-12-14 19:39:57 +1300

    Fixing tests:

--- tests/test_img_transform.py
+++ tests/test_img_transform.py
@@ -2,10 +2,12 @@ import os
 
 import whos_that_pokemon_s3gallery.img_transform
 
-test_input_image = "./sneasel.png"
-test_output_image_silhouette = "./sneasel-bw.png"
+from tests import test_dir
+
+test_input_image_orig = f"{test_dir}/sneasel.png"
+test_output_image_silhouette = f"{test_dir}/sneasel-bw.png"
 
 
 def test_create_silhouette_of_img():
-    whos_that_pokemon_s3gallery.img_transform.create_silhouette_of_img(test_input_image, test_output_image_silhouette)
+    whos_that_pokemon_s3gallery.img_transform.create_silhouette_of_img(test_input_image_orig, test_output_image_silhouette)
     os.remove(test_output_image_silhouette)
