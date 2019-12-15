65b8b09f198159cd08ef2a794a92be7c1ae00b6e
Joshua Eaton <Joshua.Eaton@airnz.co.nz>
2019-12-16 10:10:19 +1300

    Fixing the test still

--- tests/test_pokemon_assets.py
+++ tests/test_pokemon_assets.py
@@ -29,8 +29,6 @@ def test_get_pokemon_img():
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     src.pokemon_assets.download_img_from_pokemon_assets(1)
-    os.remove(f"{output_dir}bulbasaur{original_image_suffix}{saved_file_type}")
-    os.remove(f"{output_dir}bulbasaur{silhouette_image_suffix}{saved_file_type}")
     os.removedirs(output_dir)
 
 
@@ -46,11 +44,6 @@ def test_download_all_pokemon_img():
     # assert len([f for f in os.listdir(output_dir)
     #             if os.path.isfile(os.path.join(f"{output_dir}", f))]) == config["max_pokemon_id"] * 2
 
-    # Removes all the pokemon downloaded
-    test = os.listdir(output_dir)
-    for item in test:
-        if item.endswith(".png"):
-            os.remove(os.path.join(output_dir, item))
     os.removedirs(output_dir)  # Removes directory
 
 
